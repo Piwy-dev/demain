be4901c81fca5ef9fd40f2ece52ca73e1a770075
Pierre-Yves Haubruge <pierre-yves.haubruge@student.uclouvain.be>
2024-09-01 21:02:22 +0200

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, g, current_app, redirect, request
+
+def create_app(test_config=None):
+    """
+    Create and configure the app.
+    """
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    lang = 'en'
+
+   # create the pages
+    @app.route("/")
+    def index():
+        print(request.accept_languages)
+        return redirect('/{}/home'.format(lang))
+    
+    @app.route("/<lang>/home")
+    def home(lang):
+        return render_template('{}/home.html'.format(lang))
+        
+    return app
